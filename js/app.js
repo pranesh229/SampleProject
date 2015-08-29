c686c8fbd6dbc740ceb7b0e49ac70db83ca5fc1c
pranesh229 <pranesh229@gmail.com>
2015-08-29 21:35:07 +0530

    commiting new changes.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-angular.module('myApp', [])
+var phonecatApp =angular.module('myApp', ['ngRoute'])
     .controller('MainController', function($scope){
         $scope.change = function(){
             $scope.nameid = "Pranesh";
-            changetextContent();
-        }
+
+        };
+
 
     });
-function changetextContent(){
-    $("#text1").val("string");
-}
+phonecatApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'pages/login.html',
+                controller: 'MainController'
+            }).
+            when('signup.html', {
+                templateUrl: 'pages/login.html',
+                controller: 'MainController'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    }]);
